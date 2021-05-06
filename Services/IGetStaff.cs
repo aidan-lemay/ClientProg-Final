4fb06c6303783fba64f9eae1b0a64ed9ef0d6c5d
Aidan LeMay <aidanpathways12@gmail.com>
2021-05-06 13:27:10 -0400

    Adding Staff Files

---
+++ Services/IGetStaff.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project3_FinalExam.Models;
+
+namespace Project3_FinalExam.Services
+{
+    public interface IGetStaff
+    {
+        Task<List<Staff>> GetAllStaff();
+    }
+}
